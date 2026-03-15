0b6c6254d55823e34c157794e8137be5a5e45baa
hotWolf <54540046+LBBZ@users.noreply.github.com>
2026-03-15 17:34:45 +0800

    微调

--- DroneController/test/init_img_test.py
+++ DroneController/test/init_img_test.py
@@ -6,7 +6,7 @@ import airsim
 
 from DroneController.drone_camera import DroneCamera
 from DroneController.drone_motion import DroneMotion
-from DroneController.image_processing import merge_images, compress_image_to_size
+from DroneController.image_processing import merge_images, compress_image_to_size, compute_depth_index
 
 BASE_DIR = os.path.dirname(__file__)
 OUTPUT_DIR = os.path.join(BASE_DIR, "artifacts", "current", "task_step_init")
@@ -36,8 +36,8 @@ def capture_views_at_pose(x, y, z, yaw_deg = 0):
     # -----------------------------
     # 测试完成
     imgs = camera.capture_all()
-    # for name, img in imgs.items():
-    #     camera.save_image(img, os.path.join(OUTPUT_DIR, f"{name}.png"))
+    for name, img in imgs.items():
+        camera.save_image(img, os.path.join(OUTPUT_DIR, f"{name}.png"))
 
     merge_img = merge_images(imgs)
     compress_down_img = compress_image_to_size(imgs["TopDown"])
@@ -49,30 +49,30 @@ def capture_views_at_pose(x, y, z, yaw_deg = 0):
     # 2. 深度图
     # -----------------------------
     # 测试完成
-    # depths = camera.capture_all_depth()
-    # for name, depth in depths.items():
-    #     # 原始深度（米）
-    #     camera.save_depth_data(depth, os.path.join(OUTPUT_DIR, f"{name}_depth.npy"))
-    #     # 可视化深度（16-bit PNG）
-    #     camera.save_depth_image(depth, os.path.join(OUTPUT_DIR, f"{name}_depth.png"))
-    # print("深度指数越小表示障碍越近")
-    # for pose in ["Front", "Left", "TopDown", "Right", "Back"]:
-    #     depth_img = depths[pose]
-    #     depth_index = compute_depth_index(depth_img)
-    #     print(f"{pose} 深度指数: {depth_index}")
+    depths = camera.capture_all_depth()
+    for name, depth in depths.items():
+        # 原始深度（米）
+        camera.save_depth_data(depth, os.path.join(OUTPUT_DIR, f"{name}_depth.npy"))
+        # 可视化深度（16-bit PNG）
+        camera.save_depth_image(depth, os.path.join(OUTPUT_DIR, f"{name}_depth.png"))
+    print("深度指数越小表示障碍越近")
+    for pose in ["Front", "Left", "TopDown", "Right", "Back"]:
+        depth_img = depths[pose]
+        depth_index = compute_depth_index(depth_img)
+        print(f"{pose} 深度指数: {depth_index}")
 
     # -----------------------------
     # 3. Segmentation 图像
     # -----------------------------
     # 测试完成
-    # segs = camera.capture_all_seg()
-    # for name, seg in segs.items():
-    #     # RGB 可视化图
-    #     camera.save_segmentation_vis(seg, os.path.join(OUTPUT_DIR, f"{name}_seg.png"))
-    #     # 单通道 ID mask（PNG）
-    #     camera.save_segmentation_id(seg, os.path.join(OUTPUT_DIR, f"{name}_seg_id.png"))
-    #     # NPY（大模型友好）
-    #     camera.save_segmentation_npy(seg, os.path.join(OUTPUT_DIR, f"{name}_seg.npy"))
+    segs = camera.capture_all_seg()
+    for name, seg in segs.items():
+        # RGB 可视化图
+        camera.save_segmentation_vis(seg, os.path.join(OUTPUT_DIR, f"{name}_seg.png"))
+        # 单通道 ID mask（PNG）
+        camera.save_segmentation_id(seg, os.path.join(OUTPUT_DIR, f"{name}_seg_id.png"))
+        # NPY（大模型友好）
+        camera.save_segmentation_npy(seg, os.path.join(OUTPUT_DIR, f"{name}_seg.npy"))
 
 
 # -----------------------------
